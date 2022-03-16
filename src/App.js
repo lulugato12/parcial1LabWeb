adc7370c09a582cb943d3b53855a6793b342072c
Mary Laura Carmona Martínez <70768750+marzthecool@users.noreply.github.com>
2022-03-15 18:19:23 -0600

    global template v0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, 
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Layout from './layout/Layout'
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body-wrap">
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path={'/'} component={Home}></Route>
+          </Routes>
+        </Layout>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <h3>Home</h3>
+    )
+}
+
+export default Home;
+
+/*
+            <Pricing />
+            <Testimonials />
+
+*/
