1540db6b99ee0fddb879bdcb87d82ac7b8621917
Lourdes Cajica <36458387+lulugato12@users.noreply.github.com>
2022-03-22 20:22:18 -0600

    Solved merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
-import './App.css';
+import React from 'react'
 import {
   BrowserRouter as Router, 
-  Routes,
+  Switch,
   Route
 } from 'react-router-dom'
-<<<<<<< HEAD
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import Layout from './layout/Layout'
 import Home from './components/pages/Home'
 import LogIn from './components/pages/LogIn'
 import SignUp from './components/pages/SignUp'
 
-function App() {
-  return (
-    <div className="body-wrap" id="root">
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path={'/LogIn'} component={LogIn}></Route>
-            <Route path={'/SignUp'} component={SignUp}></Route>
-=======
-
-import Layout from './layout/Layout'
-import Home from './components/pages/Home'
+import './App.css';
 
 function App() {
   return (
     <div className="body-wrap">
       <Router>
         <Layout>
-          <Routes>
->>>>>>> Master
+          <Switch>
+            <Route path={'/LogIn'} component={LogIn}></Route>
+            <Route path={'/SignUp'} component={SignUp}></Route>
             <Route path={'/'} component={Home}></Route>
-          </Routes>
+          </Switch>
         </Layout>
       </Router>
     </div>
